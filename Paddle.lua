4637611ef364176148d0554a5cf044dfca3db7ee
Rob Loach <robloach@gmail.com>
2015-03-17 15:40:30 -0400

    feat(paddle): Set a max speed for the paddles

--- Paddle.lua
+++ Paddle.lua
@@ -13,6 +13,13 @@ do
         self.velocity.y = self.velocity.y * -1
         self:top(0)
       end
+      if math.abs(self.velocity.y) > self.maxSpeedY then
+        if self.velocity.y > 0 then
+          self.velocity.y = self.maxSpeedY
+        else
+          self.velocity.y = -self.maxSpeedY
+        end
+      end
       return _parent_0.update(self, dt)
     end
   }
@@ -25,7 +32,8 @@ do
       self.friction.y = 900
       self.accelerationSpeed = 1800
       self.distanceFromSide = 20
-      return self:centerY(self.game.height / 2)
+      self:centerY(self.game.height / 2)
+      self.maxSpeedY = 250
     end,
     __base = _base_0,
     __name = "Paddle",
